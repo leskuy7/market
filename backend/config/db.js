09afb321aae4f474152075c463f4a15c6f9ddfb5
leskuy7 <leskuyy7@gmail.com>
2026-03-30 22:37:54 +0300

    a

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log(`MongoDB Bağlandı: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Hata: ${error.message}`);
